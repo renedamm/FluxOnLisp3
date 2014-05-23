a28f3054cb692cda63f43d783912b4e3a8a39533
Rene Damm <rene.damm@gmx.net>
2014-05-23 04:59:04 +0200

    Implement whitespace parser.

--- fl.lisp
+++ fl.lisp
@@ -97,11 +97,12 @@
 (defgeneric scanner-at-end? (scanner))
 (defgeneric scanner-peek-next (scanner))
 (defgeneric scanner-read-next (scanner))
+(defgeneric scanner-match (scanner token))
 
 (defun make-string-scanner (string)
   (make-instance 'string-scanner :string string))
 
-(defmethod scanner-at-end? ((scanner string-scanner))
+(defmethod scanner-at-end-p ((scanner string-scanner))
   (equal (slot-value scanner 'position)
 	 (length (slot-value scanner 'string))))
 
@@ -114,32 +115,85 @@
     (incf (slot-value scanner 'position))
     token))
 
+(defmethod scanner-match ((scanner string-scanner) char)
+  (cond
+    ((scanner-at-end-p scanner) nil)
+    ((equal (scanner-peek-next scanner) char)
+     (incf (slot-value scanner 'position))
+     t)
+    (t nil)))
+
 (deftest test-string-scanner ()
   (check
-    (not (scanner-at-end? (make-string-scanner "foo")))
-    (scanner-at-end? (make-string-scanner ""))
+
+    (not (scanner-at-end-p (make-string-scanner "foo")))
+    (scanner-at-end-p (make-string-scanner ""))
+
     (let ((scanner (make-string-scanner "foo")))
       (and (equal (scanner-peek-next scanner) #\f)
 	   (equal (scanner-position scanner) 0)))
+
     (let ((scanner (make-string-scanner "foo")))
       (and (equal (scanner-read-next scanner) #\f)
-	   (equal (scanner-position scanner) 1)))))
+	   (equal (scanner-position scanner) 1)))
+    
+    (scanner-match (make-string-scanner "foo") #\f)
+    (not (scanner-match (make-string-scanner "foo") #\b))
+    (not (scanner-match (make-string-scanner "") #\f))))
 
 (defsuite test-scanners ()
   (test-string-scanner))
 
 ;;;;------------------------------- Parsers ----------------------------------------
 
-;; Every parser takes a
-
 (defclass parser-state ()
   ())
 
+(defmacro parse-result-match-p (result)
+  result)
+
+(defmacro parse-result-no-match-p (result)
+  `(not ,result))
+
+(defmacro parse-result-length (result)
+  `(third ,result))
+
+(defmacro parse-result-match (value position scanner)
+  (with-gensyms (pos)
+    `(let ((,pos ,position))
+       (list ,value ,pos (- (scanner-position scanner) ,pos)))))
+
+(defmacro parse-result-no-match ()
+  nil)
+
 (defun parse-whitespace (scanner state)
-  ())
+  (let ((saved-position (scanner-position scanner)))
+    (loop
+       (if (scanner-at-end-p scanner)
+	   (return))
+       (let ((char (scanner-peek-next scanner)))
+	 (if (or (equal char #\Newline)
+		 (equal char #\Tab)
+		 (equal char #\Space)
+		 (equal char #\Return))
+	     (scanner-read-next scanner)
+	     (return))))
+    (if (not (equal saved-position (scanner-position scanner)))
+	(parse-result-match nil saved-position scanner)
+	(parse-result-no-match))))
 
 (deftest test-parse-whitespace ()
-  t)
+  (check
+
+    (let ((scanner (make-string-scanner "foo")))
+      (and (parse-result-no-match-p (parse-whitespace scanner nil))
+	   (equal (scanner-position scanner) 0)))
+    
+    (let* ((scanner (make-string-scanner (format nil " ~C~C~Cfoo" #\Return #\Newline #\Tab)))
+	   (result (parse-whitespace scanner nil)))
+      (and (parse-result-match-p result)
+	   (equal (scanner-position scanner) 4)
+	   (equal (parse-result-length result) 4)))))
 
 (defun parse-definition ()
   ())
@@ -157,4 +211,5 @@
   (test-parse-whitespace))
 
 (defun run-tests ()
-  (test-scanners))
+  (test-scanners)
+  (test-parsers))
